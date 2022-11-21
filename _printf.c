ba4b23446dd7b5928eac3773e48ee0dc55685b55
Obed Rayo <obedrayo2356@gmail.com>
2022-11-21 11:40:53 -0800

    Add changes

--- _printf.c
+++ _printf.c
@@ -10,24 +10,21 @@
  **/
 int _printf(const char *format, ...)
 {
-	int size;
-	va_list args;
-
-	if (format == NULL)
-		exit (1);
-
-	size = _strlen(format);
-
-	if (size <= 0)
-		return (0);
-
-	va_start(args, format);
-
-	//Process here
-
-
-	//The rpocess finished here
-	va_end(args);
-
-	return (size);
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 }
--- main.h
+++ main.h
@@ -1,6 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct convert - defines a structure for symbols and functions
+ *
+ * @sym: The operator
+ * @f: The function associated
+ */
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/**
+ * Functions
+ */
 int _printf(const char *format, ...);
 
 #endif

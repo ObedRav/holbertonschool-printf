43d28621d1b7eab1e45ac4e398ea7331cbada5e1
Obed Rayo <obedrayo2356@gmail.com>
2022-11-21 20:15:49 -0500

    Fix print_binary

--- _base_functions.c
+++ _base_functions.c
@@ -13,7 +13,7 @@ int print_binary(va_list list)
     num = va_arg(list, unsigned int);
 
     if (num == 0)
-        return(_write_char('0'))
+        return(_write_char('0'));
     if (num < 1)
         return (-1):
 }
